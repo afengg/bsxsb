edfac85ee49a91e99a97d92343cfd257c328aaec
lunzhang <breakermonkey55@yahoo.com>
2015-03-18 10:57:33 -0400

    screw maven fixed lib prob myself :)

---
+++ src/java/SpringSecurity/UserService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SpringSecurity;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ *
+ * @author lun
+ */
+public class UserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String string) throws UsernameNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
