1135ada7c1e9ef91167fbbcbf6bae2decdf2b672
lunzhang <breakermonkey55@yahoo.com>
2015-03-18 20:44:08 -0400

    Revert "Revert "added logout and cant access index after login""
    This reverts commit 6284e51631fe95106a201d7aaefa8573c22d853d.

--- src/java/BSxSB/Controllers/LoginController.java
+++ src/java/BSxSB/Controllers/LoginController.java
@@ -4,7 +4,19 @@
  * and open the template in the editor.
  */
 package BSxSB.Controllers;
+
+import DAO.AdminDAO;
+import DAO.ScheduleBlockDAO;
+import DAO.SchoolDAO;
+import DAO.StudentDAO;
+import Mapping.POJO.Admins;
+import Mapping.POJO.Scheduleblocks;
+import Mapping.POJO.Schools;
+import Mapping.POJO.Students;
+import java.util.List;
 import javax.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -20,15 +32,35 @@ import org.springframework.web.portlet.ModelAndView;
 @Controller
 public class LoginController {
 
-@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String login(
-		@RequestParam(value = "error", required = false) String error,
-		@RequestParam(value = "logout", required = false) String logout) {
- 
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String login(Model model,
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        Students currentStudent = StudentDAO.getStudent(name);
+        if (currentStudent != null) {
+            model.addAttribute("student", currentStudent);
+            return "student";
+        }
+        Admins currentAdmin = AdminDAO.getAdmin(name);
+        if (currentAdmin != null) {
+            SchoolDAO schoolDAO = new SchoolDAO();
+            ScheduleBlockDAO scheduleBlockDAO = new ScheduleBlockDAO();
+            List<Schools> schools = schoolDAO.allSchools();
+            for (Schools school : schools) {
+                List<Scheduleblocks> scheduleBlocks = scheduleBlockDAO.getSchoolsScheduleBlocks(school.getSchoolid());
+                String SB2Strings = "";
+                for (Scheduleblocks sb : scheduleBlocks) {
+                    SB2Strings += sb.toString();
+                }
+                school.setScheduleblocks(SB2Strings);
+            }
+            model.addAttribute("school", schools);
+            return "admin";
+        }
+        return "index";
 
- 
-		return "index";
- 
-	}
+    }
 
 }
